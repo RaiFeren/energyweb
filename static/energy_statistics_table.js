eb567053bce45826740c4c6331f83941fd478293
Rai Feren <rai_feren@hmc.edu>
2011-05-23 14:49:07 -0700

    Started Branch with the redesign, splitting graph and table.
    Notably, splitting the Table of Statistics from the graph. This means the Javascript needs to be split into two. The table needs to remain dynamic if we want the ability to get other statistics such as the current energy usage.

--- static/energy_statistics_table.js
+++ static/energy_statistics_table.js
@@ -5,11 +5,6 @@ $(function () {
     var desired_first_record = null;
     var sg_xy_pairs = {};
     var sensor_groups = null;
-
-    function kw_tickformatter(val, axis) {
-        // Workaround to get units on the y axis
-        return val + " kW";
-    }
     
     function array_index_of(ar, x) {
         // (IE doesn't have Array.indexOf)
@@ -18,7 +13,7 @@ $(function () {
                 return i;
             }
         }
-        return -1; // Didn't find index
+        return -1;
     }
     
     function rnd(x) {
@@ -27,7 +22,8 @@ $(function () {
     }
     
     function refreshdata_json_cb(data) {
-        // Given new data from the server, update the page (graph)
+        // Given new data from the server, update the page (graph and
+        // table).
 
         // TODO: what if somehow we get no results and it's not the 
         // first time?
@@ -54,8 +50,9 @@ $(function () {
         if (first_time) {
             sensor_groups = data.sensor_groups;
 
-            // When data is received the first time,
-            // remove the graph's loading animation
+            // When data is received the first time, reveal the table 
+            // and remove the graph's loading animation
+            $('#energystats').show();
             $('#graph').empty();
         }
     
@@ -99,38 +96,49 @@ $(function () {
                 label: sensor_groups[i][1],
                 color: '#' + sensor_groups[i][2]
             });
+    
+            for (var j=0; j < sensor_groups[i][3].length; j++) {
+                sensor_id = sensor_groups[i][3][j][0];
 
-        }
-        // Finally, make the graph
-        graph_opts = {
-            series: {
-                lines: {show: true},
-                points: {show: false}
-            },
-            legend: {
-                show: true,
-                position: 'ne',
-                backgroundOpacity: 0.6,
-                noColumns: sensor_groups.length
-            },
-            yaxis: {
-                min: 0,
-                tickFormatter: kw_tickformatter,
-            },
-            xaxis: {
-                min: desired_first_record,
-                mode: 'time',
-                timeformat: '%h:%M %p',
-                twelveHourClock: true
-            },
-            grid: {
-                show: true,
-                color: '#d0d0d0',
-                borderColor: '#bbbbbb',
-                backgroundColor: { colors: ['#dbefff', '#ffffff']}
+                // update the per-sensor table averages, if appropriate
+                if (sensor_groups[i][3].length > 1) {
+                    if (sensor_id in data.week_averages) {
+                        $('#WeeklyS' + sensor_id).empty();
+                        $('#WeeklyS' + sensor_id).append(rnd(
+                            data.week_averages[sensor_id]));
+                    } else {
+                        missed_week_average = true;
+                    }
+                    if (sensor_id in data.month_averages) {
+                        $('#MonthlyS' + sensor_id).empty();
+                        $('#MonthlyS' + sensor_id).append(rnd(
+                            data.month_averages[sensor_id]));
+                    } else {
+                        missed_month_average = true;
+                    }
+                }
+    
+                // Add sensor averages to the sensor group averages...
+                if (sensor_id in data.week_averages) {
+                    group_week_average += data.week_averages[sensor_id];
+                }
+                if (sensor_id in data.month_averages) {
+                    group_month_average += data.month_averages[sensor_id];
+                }
             }
-        };
-        $.plot($('#graph'), series, graph_opts);
+    
+            // Now update table averages for this sensor group
+            if (! missed_week_average) {
+                $('#WeeklySG' + group_id).empty();
+                $('#WeeklySG' + group_id).append(
+                    rnd(group_week_average));
+            }
+            if (! missed_month_average) {
+                $('#MonthlySG' + group_id).empty();
+                $('#MonthlySG' + group_id).append(
+                    rnd(group_month_average));
+            }
+        }
     
         if (first_time) {
             first_time = false;
@@ -143,9 +151,7 @@ $(function () {
         $.getJSON(data_url, refreshdata_json_cb);
     }
 
-    // Initially, show a loading animation instead of the graph
-
-    $('#graph').append(
-        '<img class="loading" src="' + MEDIA_URL + 'loading.gif" />');
+    // Initially, hide the table
+    $('#energystats').hide();
     refreshdata();
 });
