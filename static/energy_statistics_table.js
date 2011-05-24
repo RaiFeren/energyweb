c1282f1d8c7bc278785396e30a2ba34cf1ed43fc
begerter <theentireqwerty@gmail.com>
2011-05-24 14:35:50 -0700

    adding some functions in to the file to see if they work now

--- static/energy_statistics_table.js
+++ static/energy_statistics_table.js
@@ -1,7 +1,35 @@
-$(function () {
+$(function () 
+{
   var first_time = true;
   alert("This function ran!");
 
   // Theoretically data_url should be defined.
   alert("data_url is: " + data_url);
+
+  function refreshdata_json_cb(data) 
+  {
+    // Given new data from the server, update the table
+
+    // TODO: what if somehow we get no results and it's not the 
+    // first time?
+    if (first_time && data.no_results) 
+    {
+      alert("we received no data :(");
+      return; // TODO: tell the user what happened?
+    }
+
+    // When this function is first called, it is expected that 
+    // data_url was defined previously (before loading this file).
+    data_url = data.data_url;
+    alert("we have a data_url which is: " + data_url);
+  }
+
+  function refreshdata() 
+  {
+    $.getJSON(data_url, refreshdata_json_cb);
+  }
+
+  // Start getting data!
+  refreshdata();
+
 })();
