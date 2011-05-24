e9756559585659f42795ba85a2823cd8a0190648
Rai Feren <rai_feren@hmc.edu>
2011-05-24 11:09:18 -0700

    Separating Table and Graph
    Actually removed them from each other... The pages are distinct now.

--- static/dynamic_graph.js
+++ static/dynamic_graph.js
@@ -21,11 +21,6 @@ $(function () {
         return -1;
     }
     
-    function rnd(x) {
-        // (Used to format numbers in the table)
-        return x.toFixed(2);
-    }
-    
     function refreshdata_json_cb(data) {
         // Given new data from the server, update the page (graph and
         // table).
@@ -55,17 +50,12 @@ $(function () {
         if (first_time) {
             sensor_groups = data.sensor_groups;
 
-            // When data is received the first time, reveal the table 
-            // and remove the graph's loading animation
-            $('#energystats').show();
+            // When data is received the first time,
+            // remove the graph's loading animation
             $('#graph').empty();
         }
     
         for (var i=0; i < sensor_groups.length; i++) {
-            group_week_average = 0;
-            group_month_average = 0;
-            missed_week_average = false;
-            missed_month_average = false;
             group_id = sensor_groups[i][0];
     
             if (first_time) {
@@ -101,48 +91,6 @@ $(function () {
                 label: sensor_groups[i][1],
                 color: '#' + sensor_groups[i][2]
             });
-    
-            for (var j=0; j < sensor_groups[i][3].length; j++) {
-                sensor_id = sensor_groups[i][3][j][0];
-
-                // update the per-sensor table averages, if appropriate
-                if (sensor_groups[i][3].length > 1) {
-                    if (sensor_id in data.week_averages) {
-                        $('#WeeklyS' + sensor_id).empty();
-                        $('#WeeklyS' + sensor_id).append(rnd(
-                            data.week_averages[sensor_id]));
-                    } else {
-                        missed_week_average = true;
-                    }
-                    if (sensor_id in data.month_averages) {
-                        $('#MonthlyS' + sensor_id).empty();
-                        $('#MonthlyS' + sensor_id).append(rnd(
-                            data.month_averages[sensor_id]));
-                    } else {
-                        missed_month_average = true;
-                    }
-                }
-    
-                // Add sensor averages to the sensor group averages...
-                if (sensor_id in data.week_averages) {
-                    group_week_average += data.week_averages[sensor_id];
-                }
-                if (sensor_id in data.month_averages) {
-                    group_month_average += data.month_averages[sensor_id];
-                }
-            }
-    
-            // Now update table averages for this sensor group
-            if (! missed_week_average) {
-                $('#WeeklySG' + group_id).empty();
-                $('#WeeklySG' + group_id).append(
-                    rnd(group_week_average));
-            }
-            if (! missed_month_average) {
-                $('#MonthlySG' + group_id).empty();
-                $('#MonthlySG' + group_id).append(
-                    rnd(group_month_average));
-            }
         }
         // Finally, make the graph
         graph_opts = {
@@ -186,9 +134,7 @@ $(function () {
         $.getJSON(data_url, refreshdata_json_cb);
     }
 
-    // Initially, hide the table and show a loading animation instead of
-    // the graph
-    $('#energystats').hide();
+    // Initially, show a loading animation instead of the graph
     $('#graph').append(
         '<img class="loading" src="' + MEDIA_URL + 'loading.gif" />');
     refreshdata();
