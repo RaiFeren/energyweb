c698e0f6ddcdcd9b079adf0f376556813a1c6f00
begerter <theentireqwerty@gmail.com>
2011-05-24 17:34:33 -0700

    Edited graph/views.py via GitHub

--- graph/views.py
+++ graph/views.py
@@ -188,7 +188,8 @@ def data_interface(request):
     #start_dt = datetime.datetime.now() - datetime.timedelta(0, 3600*3, 0)
     #data = str(int(calendar.timegm(start_dt.timetuple()) * 1000))
     return render_to_response('graph/data_interface.html',
-        {'sensor_groups': sensor_groups},
+        {'sensor_groups': sensor_groups,
+         'sensors': sensor_ids},
         context_instance=RequestContext(request))
     
 
