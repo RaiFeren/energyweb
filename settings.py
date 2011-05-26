51debfc3e0baee8a10f7991360da1a895c27502b
begerter <theentireqwerty@gmail.com>
2011-05-26 14:53:54 -0700

    Edited settings.py via GitHub

--- settings.py
+++ settings.py
@@ -55,7 +55,7 @@ MEDIA_ROOT = '/var/local/energyweb/static/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://localhost:80/static/'
+MEDIA_URL = 'http://localhost:8000/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
