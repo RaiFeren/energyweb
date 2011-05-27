d2127799bd16e57f6598ffae697342b55acd3ab3
begerter <theentireqwerty@gmail.com>
2011-05-27 15:26:34 -0700

    Edited static/energy_statistics_table.js via GitHub

--- static/energy_statistics_table.js
+++ static/energy_statistics_table.js
@@ -6,6 +6,10 @@ $(function ()
   // Theoretically data_url should be defined.
   alert("data_url is: " + data_url);
 
+    $('td.nameatwood').click(function() {
+	alert('You clicked on the name atwood');
+    });
+
   function refreshdata_json_cb(data) 
   {
 
@@ -24,75 +28,12 @@ $(function ()
       alert("we received no data :(");
       return; // TODO: tell the user what happened?
     }
-        var series_opts = [];
-        var series = [];
-        var missed_week_average, 
-            missed_month_average, 
-            sensor_id,
-            group_id,
-            group_week_average, 
-            group_month_average, 
-
-        if (first_time) {
-            sensor_groups = data.sensor_groups;
-        }
     
-        for (var i=0; i < sensor_groups.length; i++) {
-            alert("on sensor number: " + i);
-            group_week_average = 0;
-            group_month_average = 0;
-            missed_week_average = false;
-            missed_month_average = false;
-            group_id = sensor_groups[i][0];
+    if (first_time) {
+        first_time = false;
+    }
     
-            for (var j=0; j < sensor_groups[i][3].length; j++) {
-                sensor_id = sensor_groups[i][3][j][0];
-
-                // update the per-sensor table averages, if appropriate
-                if (sensor_groups[i][3].length > 1) {
-                    if (sensor_id in data.week_averages) {
-                        $('#WeeklyS' + sensor_id).empty();
-                        $('#WeeklyS' + sensor_id).append(rnd(
-                            data.week_averages[sensor_id]));
-                    } else {
-                        missed_week_average = true;
-                    }
-                    if (sensor_id in data.month_averages) {
-                        $('#MonthlyS' + sensor_id).empty();
-                        $('#MonthlyS' + sensor_id).append(rnd(
-                            data.month_averages[sensor_id]));
-                    } else {
-                        missed_month_average = true;
-                    }
-                }
-    
-                // Add sensor averages to the sensor group averages...
-                if (sensor_id in data.week_averages) {
-                    group_week_average += data.week_averages[sensor_id];
-                }
-                if (sensor_id in data.month_averages) {
-                    group_month_average += data.month_averages[sensor_id];
-                }
-            }
-    
-            // Now update table averages for this sensor group
-            if (! missed_week_average) {
-                $('#WeeklySG' + group_id).empty();
-                $('#WeeklySG' + group_id).append(
-                    rnd(group_week_average));
-            }
-            if (! missed_month_average) {
-                $('#MonthlySG' + group_id).empty();
-                $('#MonthlySG' + group_id).append(
-                    rnd(group_month_average));
-            }
-        }
-    
-        if (first_time) {
-            first_time = false;
-        }
-    
-        setTimeout(refreshdata, 10000);
+    setTimeout(refreshdata, 10000);
   }
 
   function refreshdata() 
@@ -103,4 +44,4 @@ $(function ()
   // Start getting data!
   refreshdata();
 
-})();
+});
